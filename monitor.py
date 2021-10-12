c546cd41aa0e5c24d536b83ebb49aa2a40f7c710
Anton <anton.gritsevich@gmail.com>
2021-10-12 17:51:27 +0300

    last changes

--- monitor.py
+++ monitor.py
@@ -13,26 +13,34 @@ st = speedtest.Speedtest()
 
 while 1:
     f = open(file_name, "a")
-
     now = datetime.datetime.now()
     data = "{0:_>20}".format(now.strftime("%Y-%m-%d %H:%M:%S"))
     download = "{0:>20}".format(st.download())
     upload = "{0:>20}".format(st.upload())
     ping = "{0:>5}".format(st.results.ping)
     f.writelines("%s. d/u/p = %s / %s / %s\n" % (data, download, upload, ping))
+    print("%s. d/u/p = %s / %s / %s\n" % (data, download, upload, ping))
 
     f.close()
 
     # git ones per day
-    command=["git", "commit", file_name, "-m", data, "&&", "git", "push", "origin", "HEAD"]
-    proc=subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("commiting\n")
+    command_commit=["git", "commit", file_name, "-m", data]
+    command_push=["git", "push", "origin", "HEAD"]
+    proc1 = subprocess.Popen(command_commit, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc2 = subprocess.Popen(command_push, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     try:
-        out, err = proc.communicate(timeout=timeout)
-        if proc.returncode == 0:
-            print()
+        out, err = proc1.communicate(timeout = timeout)
+        print(out)
+        print(err)
+        out, err = proc2.communicate(timeout = timeout)
+        print(out)
+        print(err)
+        #if proc.returncode == 0:
+            #print()
     except subprocess.TimeoutExpired:
-        proc.kill()
+        proc1.kill()
 
-    #slepp for 1h
+    print("slepp for 1h")
     time.sleep(3600) # Sleep for 3 seconds
