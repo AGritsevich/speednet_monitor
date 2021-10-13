56cd19de2913fd1ddf689eac948a5dc63486fbe0
Anton <anton.gritsevich@gmail.com>
2021-10-13 15:20:43 +0300

    Clean log message

--- monitor.py
+++ monitor.py
@@ -6,26 +6,25 @@ import time
 import subprocess
 
 file_name = "monitor.data"
-timeout = 10
 st = speedtest.Speedtest()
-#st.get_best_server(st.set_mini_server("http://speedtest.test.fr/"))
-#st.get_best_server()
 
 while 1:
     f = open(file_name, "a")
     now = datetime.datetime.now()
-    data = "{0:_>20}".format(now.strftime("%Y-%m-%d %H:%M:%S"))
-    download = "{0:>20}".format(st.download())
-    upload = "{0:>20}".format(st.upload())
-    ping = "{0:>5}".format(st.results.ping)
-    f.writelines("%s. d/u/p = %s / %s / %s : %s\n" % (data, download, upload, ping, st.get_best_server()))
-    print("%s. d/u/p = %s / %s / %s : %s \n" % (data, download, upload, ping, st.get_best_server()))
+    date = "{0:>19}".format(now.strftime("%Y-%m-%d %H:%M:%S"))
+    download = "{0:>18}".format(st.download())
+    upload = "{0:>18}".format(st.upload())
+    ping = "{0:>6}".format(st.results.ping)
+
+    f.writelines("%s. d/u/p = %s / %s / %s : %s\n" % (date, download, upload, ping, st.results.server['host']))
+    print("%s. d/u/p = %s / %s / %s : %s \n" % (date, download, upload, ping, st.results.server['host']))
 
     f.close()
 
     # git ones per day
-    print("commiting\n")
-    command_commit=["git", "commit", file_name, "-m", data]
+    print("commiting")
+    timeout = 10 # sec
+    command_commit=["git", "commit", file_name, "-m", date]
     command_push=["git", "push", "origin", "HEAD"]
     proc1 = subprocess.Popen(command_commit, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     proc2 = subprocess.Popen(command_push, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -42,5 +41,5 @@ while 1:
     except subprocess.TimeoutExpired:
         proc1.kill()
 
-    print("slepp for 1h")
-    time.sleep(3600) # Sleep for 3 seconds
+    print("sleep for 1h")
+    time.sleep(3600)
