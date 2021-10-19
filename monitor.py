ff50fb42d90eb41cf533338b539d4ef65b48892c
Anton <anton.gritsevich@gmail.com>
2021-10-19 12:58:52 +0300

    New version in Mbps

--- monitor.py
+++ monitor.py
@@ -7,39 +7,47 @@ import subprocess
 
 file_name = "monitor.data"
 st = speedtest.Speedtest()
+Mbps = 1000*1000
 
 while 1:
-    f = open(file_name, "a")
-    now = datetime.datetime.now()
-    date = "{0:>19}".format(now.strftime("%Y-%m-%d %H:%M:%S"))
-    download = "{0:>18}".format(st.download())
-    upload = "{0:>18}".format(st.upload())
-    ping = "{0:>6}".format(st.results.ping)
+    try:
+        f = open(file_name, "a")
+        now = datetime.datetime.now()
+        date = "{0:>19}".format(now.strftime("%Y-%m-%d %H:%M:%S"))
+        download = "{0:>3.3f} Mbps".format(st.download() / Mbps )
+        upload = "{0:>3.3f} Mbps".format(st.upload() / Mbps)
+        ping = "{0:>2} ms".format(st.results.ping)
 
-    f.writelines("%s. d/u/p = %s / %s / %s : %s\n" % (date, download, upload, ping, st.results.server['host']))
-    print("%s. d/u/p = %s / %s / %s : %s \n" % (date, download, upload, ping, st.results.server['host']))
+        f.writelines(f"{date}. d/u/p = {download} / {upload} / {ping} : {st.results.server['host']}\n")
+        print(f"{date}. d/u/p = {download} / {upload} / {ping} : {st.results.server['host']}\n")
 
-    f.close()
+        f.close()
+    except:
+        print =("Test period have some fail. Doesn't care. Go on.")
 
     # git ones per day
     print("commiting")
     timeout = 10 # sec
     command_commit=["git", "commit", file_name, "-m", date]
     command_push=["git", "push", "origin", "HEAD"]
-    proc1 = subprocess.Popen(command_commit, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    proc2 = subprocess.Popen(command_push, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    commit = subprocess.Popen(command_commit, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    push = subprocess.Popen(command_push, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     try:
-        out, err = proc1.communicate(timeout = timeout)
+        out = commit.communicate(timeout = timeout)
+        print(f"Commit out:")
         print(out)
-        print(err)
-        out, err = proc2.communicate(timeout = timeout)
+        
+        out = push.communicate(timeout = timeout)
+        print(f"Push out:")
         print(out)
-        print(err)
         #if proc.returncode == 0:
             #print()
     except subprocess.TimeoutExpired:
-        proc1.kill()
+        commit.kill()
+        push.kill()
+    except :
+        print =("Commit period have some fail. Doesn't care. Go on.")
 
     print("sleep for 1h")
     time.sleep(3600)
